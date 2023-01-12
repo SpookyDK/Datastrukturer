1d0f811a0b0f49fa125b1d0114ad8736edefbdb9
Asbjørn Pedersen <Asbjrn Pedersen@DESKTOP-PFQ8HJR>
2023-01-12 21:29:14 +0100

    Add project files.

---
+++ Datastrukturer/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Datastrukturer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int indx = 3;
+            string[] strang = {"kage","sovs","pap","ur","tv","lys","led"};
+            string tilsat = "ord";
+        }
+        static private (bool, string[]) Op_3(int indx, string tilsat, string[] strang)
+        {
+            string[] temp = new string[strang.Length + 1];
+            if (indx <= temp.Length)
+            {
+            for (int i = 0; i <= temp.Length; i++)
+            {
+                if (i < indx)
+                {
+                    temp[i] = strang[i];
+
+                }
+                else if (i == indx)
+                {
+                    temp[i] = tilsat;
+                }
+                else
+                {
+                    temp[i] = strang[i-1];
+                }
+            }
+            strang= temp;
+            return (true, strang);
+            }
+            else { return (false, strang); }
+        }
+    }
+}
